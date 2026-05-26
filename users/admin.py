0cf6e2a573465aafdeb57f740747c45f863ef6fe
Davis Crown <davis21crown@gmail.com>
2026-05-27 01:49:55 +0200

    feat: user model class

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username','email','phone_number','country','trust_score','date_joined']
+    search_fields = ['username','email','phone_number']
+    list_filter = ['country', 'is_staff','is_active']
+    ordering=['-date_joined']
+    
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     """Custom User model for Africa Trust System"""
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True)
+    trust_score = models.IntegerField(default=0)
     class Meta:
         verbose_name="User"
         verbose_name_plural="Users"
