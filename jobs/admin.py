0423e23cd20b803810d4f9ae4f4f7edf7929316a
Davis Crown <davis21crown@gmail.com>
2026-05-27 15:15:10 +0200

    feat: implement Job model and admin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Job
 # Register your models here.
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['title','description', 'location', 'created_at', 'client', 'budget', 'status']
+    search_fields = ['title', 'client__username' , 'location']
+    list_filter = ['status', 'location']
+    
+admin.site.register(Job, JobAdmin)
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Job(models.Model):
+    """Job models for Africa Trust System"""
+    STATUS_CHOICES = [
+        ('OPEN', 'Open'),
+        ('ASSIGNED', 'Assigned'),
+        ('COMPLETED', 'Completed'),
+    ]
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    client = models.ForeignKey(settings.AUTH_USER_MODEL, related_name = 'job_postings', on_delete = models.CASCADE)
+    budget =  models.DecimalField(max_digits = 10, decimal_places = 2)
+    status = models.CharField(max_length=30, choices = STATUS_CHOICES, default = 'OPEN')
+    class Meta:
+        verbose_name = 'Job'
+        verbose_name_plural = 'Jobs'
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f"{self.title} - {self.status}"
+        
