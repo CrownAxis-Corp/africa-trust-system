053701e597c6805bbf6d0a782efed48d7faead2b
Davis Crown <davis21crown@gmail.com>
2026-05-27 22:47:16 +0200

    feat: implement trust model and admin

--- trust/admin.py
+++ trust/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TrustEvent
 
 # Register your models here.
+class TrustAdmin(admin.ModelAdmin):
+    list_display = ['user', 'event_type', 'score_change', 'notes']
+    search_fields = ['user__username']
+    list_filter = ['event_type', 'created_at']
+    
+admin.site.register(TrustEvent, TrustAdmin)
--- trust/models.py
+++ trust/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class TrustEvent(models.Model):
+    """TrustEvent fields for Africa Trust System"""
+    EVENT_CHOICES=[
+        ("DISPUTE_LOST","Dispute_lost"),
+        ("NO_SHOW", "No_show"),
+        ("EARLY_COMPLETION", "Early_completion"),
+        ("PAYMENT_ON_TIME", "Payment_on_time"),
+    ]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='User_Trust', on_delete=models.CASCADE)
+    event_type = models.CharField(max_length=50, choices = EVENT_CHOICES, default = "NO_SHOW")
+    score_change = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    notes = models.TextField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = "TrustEvent"
+        verbose_name_plural = "TrustEvents"
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return f"{self.user} - {self.event_type}"
