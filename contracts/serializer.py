4198ceeeec70c51e0711604c9ed22c2ba7fc6f4f
Davis Crown <davis21crown@gmail.com>
2026-05-30 18:40:45 +0200

    feat: add ContractSerializer and ContractViewSet

---
+++ contracts/serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Contract
+from users.serializers import UserSerializer
+from users.models import User
+class ContractSerializer(serializers.ModelSerializer):
+    client = UserSerializer(read_only=True)
+    contractor_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    client_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    contractor = UserSerializer(read_only=True)
+    class Meta:
+        model = Contract
+        fields = ['title','status','description', 'amount', 'start_date', 'end_date', 'client', 'contractor', 'contractor_id', 'client_id']
---
+++ contracts/urls.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import DefaultRouter
+from .views import ContractViewSet
+
+router = DefaultRouter()
+router.register(r'contracts', ContractViewSet, basename='contract')
+urlpatterns = router.urls
