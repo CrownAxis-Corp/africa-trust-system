087e98882d5ef5b9885a5271431d764224a39dcc
DavisCrown <davis21crown@gmail.com>
2026-07-07 16:51:38 +0200

    Feat: RegisterView with duplicate username handling and registration tests

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, LoginView
+from .views import UserViewSet, LoginView, RegisterView
 from django.urls import path
 
 router = DefaultRouter()
@@ -7,5 +7,6 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = router.urls + [
-    path('auth/login/', LoginView.as_view(), name='login')
+    path('auth/login/', LoginView.as_view(), name='login'),
+    path('auth/register/', RegisterView.as_view(), name='register')
 ]
--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate, get_user_model
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
+from django.db import IntegrityError
 
 
 # Create your views here.
@@ -13,7 +14,6 @@ from rest_framework.permissions import AllowAny
 class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-
 
 class LoginView(APIView):
     authentication_classes = []
@@ -31,16 +31,20 @@ class LoginView(APIView):
         token, created = Token.objects.get_or_create(user=user)
         return Response({'token': token.key})
     
-
 class RegisterView(APIView):
     authentication_classes = []
     permission_classes = [AllowAny]
+
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
 
-        if username, password is None:
-            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key})
+        if username is None or password is None:
+            return Response({'error': 'Username and Password are required'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            User = get_user_model()
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return Response({'error':'user already exists'}, status=status.HTTP_400_BAD_REQUEST)
+        token,created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_201_CREATED) 
