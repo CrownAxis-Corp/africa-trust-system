65948390b3823a68e942c07d8728e3054e53627d
DavisCrown <davis21crown@gmail.com>
2026-07-13 16:18:27 +0200

    Fix: removed client_id to prevent impersonation and mapped contractor_id to contractor

--- contracts/serializer.py
+++ contracts/serializer.py
@@ -4,9 +4,8 @@ from users.serializers import UserSerializer
 from users.models import User
 class ContractSerializer(serializers.ModelSerializer):
     client = UserSerializer(read_only=True)
-    contractor_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    client_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    contractor_id = serializers.PrimaryKeyRelatedField(source='contractor',queryset=User.objects.all())
     contractor = UserSerializer(read_only=True)
     class Meta:
         model = Contract
-        fields = ['title','status','description', 'amount', 'start_date', 'end_date', 'client', 'contractor', 'contractor_id', 'client_id']
+        fields = ['title','status','description', 'amount', 'start_date', 'end_date', 'client', 'contractor', 'contractor_id']
